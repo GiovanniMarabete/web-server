2df06c293065158dfa94524a1e690e56bd1de8c7
Giovanni Marabete <gmarabete@gmail.com>
2016-10-30 20:13:26 +0000

    Small changes

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ var express = require('express');
 var app = express();
 var PORT = '3000';
 
-//app.get('/', function (req , res){
-    //res.send('Hello Express!');
-//});
-
-//var meta  = new AWS.MetadataService();
-
-//meta.request("/latest/meta-data/instance-id", function(err, data){
-   // console.log(data);
-//});
-
 app.get('/about', function (req , res){
     res.send('We are The Best!!!');
 });
