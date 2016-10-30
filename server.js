9f61ddf5806250d542426d510ca73ebdd2e98b7a
Giovanni Marabete <gmarabete@gmail.com>
2016-10-30 19:10:09 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var PORT = '3000';
+
+//app.get('/', function (req , res){
+    //res.send('Hello Express!');
+//});
+
+app.get('/about', function (req , res){
+    res.send('We are The Best!!!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function(){
+    console.log('We are listening on Port: ' + PORT);
+});
+
+
